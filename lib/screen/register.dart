96652b394a1401d0b112fc0e231558477910707f
Ida Bagus Krisna <dbaguskrisna@gmail.com>
2022-09-03 14:43:14 +0700

    adding email validation at register page

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -201,6 +201,13 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               ),
             ),
             Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Data Login',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                )),
+            Container(
               padding: EdgeInsets.all(10),
               child: TextFormField(
                 controller: alamat_email,
@@ -211,47 +218,13 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter some text';
+                  } else {
+                    if (isValid == true) {
+                      return null;
+                    } else {
+                      return 'Email tidak valid';
+                    }
                   }
-                  return null;
-                },
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                controller: konfirmasi_alamat_email,
-                decoration: InputDecoration(
-                    hintText: 'Konfirmasi Alamat Email',
-                    border: OutlineInputBorder(),
-                    labelText: 'Konfirmasi Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Data Login',
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                )),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Masukkan Username',
-                    border: OutlineInputBorder(),
-                    labelText: 'Username'),
-                controller: username,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
                 },
               ),
             ),
@@ -349,9 +322,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   child: Text('Daftar Baru'),
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                      print(konfirmasi_alamat_email.text);
-                      print(alamat_email.text);
-                      if (konfirmasi_alamat_email.text == alamat_email.text) {
+                      isValid =
+                          EmailValidator.validate(alamat_email.text.toString());
+                      if (isValid == true) {
                         if (password.text == konfirmasiPassword.text) {
                           submit();
                         } else {
@@ -361,7 +334,9 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                         }
                       } else {
                         ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Alamat email tidak sama')),
+                          SnackBar(
+                              content: Text(
+                                  'Alamat email tidak valid, silahkan masukkan alamat email dengan benar')),
                         );
                       }
                     } else {
